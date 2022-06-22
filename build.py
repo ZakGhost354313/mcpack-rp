9bfdd286585a61fddb45cce5b098a106bad30b28
Zak Dahlia <MBM3543@gmail.com>
2022-06-22 18:34:46 +0000

    added some uuids, and other things

---
+++ build.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+from time import sleep
+
+def build():
+    os.system('zip -r pack.zip pack')
+    os.system('mv pack.zip wp_BP.mcpack')
+
+def main():
+    a = input('you sure(y/n)?  ') 
+    if a == "y": build()
+    elif a == "n": print('bye')
+    else: 
+        print('error')
+        main()
+
+if __name__ == "__main__": 
+    main()
